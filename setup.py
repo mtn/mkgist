1fdb12b5e7973016c12aa7b229572689c18330df
Michael Noronha <michaeltnoronha@gmail.com>
2018-07-15 12:31:36 -0700

    Prepare for release on pypi

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mkgist",
+    version="1.0",
+    author="Michael Noronha",
+    author_email="michaeltnoronha@gmail.com",
+    description="A small utility for making gists",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mtn/mkgist",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    scripts=["bin/mkgist"],
+)
